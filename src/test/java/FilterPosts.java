212b24efa439b3281316af6d6a8403371c208b5a
krypd <krypel.dominik@gmail.com>
2022-01-05 16:43:24 +0100

    addQueryParamsTests

--- src/test/java/FilterPosts.java
+++ src/test/java/FilterPosts.java
@@ -1,15 +1,16 @@
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import static io.restassured.RestAssured.given;
 
 public class FilterPosts {
 
     @Test
     public void filterPosts() {
-        given().log().all().queryParam("author", "Dominik").
+        given().log().all().queryParam("author", "Dominik").  // stosujemy queryParam, chcemy znaleźć posty, których autorem jest Dominik
                 when().get("http://localhost:3000/posts").
                 then().log().all(); // jak chcemy logować odpowiedź na post-a, to w sekcji then wstawiamy wartość ".log().all()"
-        // stosujemy queryParam, chcemy znaleźć posty, których autorem jest Dominik
     }
 
     @Test
@@ -17,5 +18,17 @@ public class FilterPosts {
         given().log().all().queryParam("id", "5","6"). // szukamy posta z id 5 i 6
                 when().get("http://localhost:3000/posts").
                 then().log().all();
+    }
+
+    @Test
+    public void filterPostsAuthorTitle() {
+        Map<String,Object> params = new HashMap<>();
+        params.put("author","Tomek");
+        params.put("title","Pierwszy POST z plik");
+
+        given().log().all().queryParams(params). // tutaj szukamy parametrów więc wskazujemy liczbę mnogą : .queryParams
+                when().get("http://localhost:3000/posts").
+                then().log().all();
+        //w taki sposób możemy za pomocą Rest Assured korzystać z query params
     }
 }
--- src/test/java/GetSpecificPostTest.java
+++ src/test/java/GetSpecificPostTest.java
@@ -8,9 +8,9 @@ public class GetSpecificPostTest {
 
     @Test
     public void getPost(){
-      given().log().all().pathParam("postId", "4").
-              when().get("http://localhost:3000/posts/{postId}").
-              then().log().all(); // definiujemy metodę pathParam po to, żeby nie hardkodować numeru post-a
+      given().log().all().pathParam("postId", "4"). // definiujemy metodę pathParam po to, żeby nie hardkodować numeru post-a
+              when().get("http://localhost:3000/posts/{postId}"). // wartość {postId} musi być taka sama jak w linijce wyżej : pathParam("postId", "4")
+              then().log().all();
         // dzieki metodzie ".log().all()" wypisujemy na konsoli/terminalu wszystko, również nagłówki, w tym przypadku dla konkretnego POST-a
 
       given().log().all().
