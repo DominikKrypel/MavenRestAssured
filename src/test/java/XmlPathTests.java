cb9170e6605ed441985ca6cd37a18a48c43e8674
krypd <krypel.dominik@gmail.com>
2022-02-14 13:59:45 +0100

    addXmlPathTests

--- src/test/java/XmlPathTests.java
+++ src/test/java/XmlPathTests.java
@@ -1,6 +1,8 @@
 import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.element.Node;
 import org.testng.annotations.Test;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class XmlPathTests {
@@ -25,8 +27,27 @@ public class XmlPathTests {
                 "\t</film>\n" +
                 "</filmy>";
 
-        String nazwa = XmlPath.from(xml).get("filmy.film.nazwa[0]");  // wyciągamy nazwę pierwszego filmu
+        String nazwa = XmlPath.from(xml).get("filmy.film.nazwa[0]");  /*wyciągamy nazwę pierwszego filmu*/
         List<String> nazwy = XmlPath.from(xml).getList("filmy.film.nazwa");
-        String gatunek = XmlPath.from(xml).get("filmy.film[0].@gatunek"); // odwołujemy się do pierwszego filmu "[0]", a następnie korzystamy ze znaczka atrybut "@"
+        String gatunek = XmlPath.from(xml).get("filmy.film[0].@gatunek");  /*odwołujemy się do pierwszego filmu "[0]",
+                                                                           a następnie korzystamy ze znaczka atrybut "@"*/
+
+        List<Node> filmy = XmlPath.from(xml).get("filmy.film.findAll {element -> return element}"); /*zwracamy wszystkie nody,
+                                                                                                    wszystkie gałęzie, które są oznaczone jako film*/
+
+        String nazwaFilmu = filmy.get(2).get("nazwa").toString(); /*wyciągamy z listy nazwę filmu*/
+
+        List<Node> komedie = XmlPath.from(xml).get("filmy.film.findAll {film -> film.@gatunek =='komedia'}"); /*dla każdego elementu sprawdzamy gatunek, i wartość
+                                                                                                               i wartrość tego atrybutu ma być równa komedia*/
+
+        Node filmOcenaDziewiec = XmlPath.from(xml).get("filmy.film.find {film -> def ocena = film.ocena; ocena =='9'}"); /*korzystamy z metody find czyli szukamy pojendynczego elementu.
+                                                                                                       Możemy zdefiniować zmienną ocena i ta ocena ma być równa 9*/
+
+        List<Node> wiekszeNizSiedem = XmlPath.from(xml).get("filmy.film.findAll {film -> def ocena = film.ocena.toFloat(); ocena > 7}");/*znajdziemy wszystkie filmy,
+                                                                                                                         które będą miały ocenę większą od 7*/
+
+        List<String> nazwyCollect = XmlPath.from(xml).get("filmy.film.collect {it.nazwa}"); /*mamy już nazwy filmów, tylko tym razem skorzystamy z metody collect*/
+
+
     }
 }
