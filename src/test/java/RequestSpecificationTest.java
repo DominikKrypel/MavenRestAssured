ac66f164629de75484077f99ffa558ea61157841
krypd <krypel.dominik@gmail.com>
2022-02-14 17:44:20 +0100

    change RequestSpecification Tests for logging

--- src/test/java/RequestSpecificationTest.java
+++ src/test/java/RequestSpecificationTest.java
@@ -1,12 +1,15 @@
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class RequestSpecification {
+public class RequestSpecificationTest {
 
     io.restassured.specification.RequestSpecification spec;
     ResponseSpecification resSpec;
@@ -20,6 +23,9 @@ public class RequestSpecification {
         resSpec = new ResponseSpecBuilder() /*tworzymy obiekt klasy response specification*/
                 .expectStatusCode(200)
                 .build();
+        ResponseLoggingFilter respoFilter = new ResponseLoggingFilter();
+        RequestLoggingFilter reqpoFilter = new RequestLoggingFilter();
+        RestAssured.filters(reqpoFilter,respoFilter); /*tutaj ustawiłem globalnie filtr, w tym przypadku ustawiamy logowanie dla responsów i requestów */
     }
 
     @Test
@@ -29,7 +35,7 @@ public class RequestSpecification {
         when()
                 .get().
         then()
-                .log().body().spec(resSpec);
+                .spec(resSpec);
     }
 
     @Test
@@ -39,6 +45,6 @@ public class RequestSpecification {
         when()
                 .get("/4"). /* tutaj metoda GET stworzy cały endpoint, czyli kombinaja trzech elementów*/
         then()
-                .log().body().spec(resSpec);
+                .spec(resSpec);
     }
 }
